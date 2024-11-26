cf7cb1184ff7e0d26a11943a626815ba955469e9
BraveCaperCat2 <113925022+BraveCaperCat2@users.noreply.github.com>
2024-11-26 15:43:22 +0000

    Update solar-system.lua

--- asteroid_definitions/solar-system.lua
+++ asteroid_definitions/solar-system.lua
@@ -26,7 +26,8 @@ local DefinitionsUtil = require("asteroid_definitions.definitions_util")
 -- Planets are sorted by the distance from the star they orbit.
 -- For instance, Earth is labelled under 3 - meaning that it's the third planet that orbits the Sun.
 -- Nil is currently representing planets which don't exist yet, such as the outer asteroid belt. These nils are defined in their respective asteroid definitions file.
-local Planets = {[1] = Mercury,
+local Planets = {
+	[1] = Mercury,
 	[2] = Venus,
 	[3] = Earth,
 	[4] = Mars,
